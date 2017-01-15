f9435dddb9555ed42370bf36fca6d6e15c16020f
Alex Claman <claman@alexclaman.com>
2017-01-15 16:08:31 -0600

    grab syntax tables

--- greek-lookup.py
+++ greek-lookup.py
@@ -36,7 +36,7 @@ URL_BASE = "http://www.perseus.tufts.edu/hopper/morph"
 
 def lookup_word(word):
     # Takes string
-    # Returns a BeautifulSoup object containing the html of the 
+    # Returns a BeautifulSoup object containing the html of the
     # Perseus Word Study Tool entry for that word; or
     # if the word is not found, returns -1
     # Get html using Requests and Beautiful Soup
@@ -53,36 +53,35 @@ def lookup_word(word):
 def get_definitions(soup):
     # Take BeautifulSoup object
     # Returns a list of tuples with the structure (lemma, lemma_definition)
-    
+
     lemmas = soup.find_all('h4',class_='greek')
     lemma_definitions = soup.find_all('span',class_='lemma_definition')
-    syntax = soup.find_all('td', class_='greek')
+    syntax = soup.find_all('table')
 
-    for item in syntax:
-        print(item.text.strip() + ' - ' + item.next_sibling.next_sibling.text.strip())
+    syntax = syntax[1:]
 
     lemma_list, lemma_definition_list = [], []
-    
+
     for lemma in lemmas:
         lemma_list.append(lemma.text.strip())
-            
+
     for lemma_definition in lemma_definitions:
         lemma_definition_list.append(lemma_definition.text.strip())
-    
+
     definitions = zip(lemma_list,lemma_definition_list)
- 
+
     return definitions
-    
+
 def main(query):
     soup = lookup_word(query)
-    
+
     if soup == -1:
         print("No definition found for " + query + ".")
     else:
         definitions = get_definitions(soup)
         for definition in definitions:
             print(query + " > "+definition[0]+": "+definition[1])
-        
+
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         print("No argument given.")
